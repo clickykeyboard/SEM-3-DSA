bc8c5b7ca305ee38e3ac382a0efc208ec7930849
itsbadr <68023752+cloudswritecode@users.noreply.github.com>
2021-04-08 23:27:33 +0500

    create a menu for the operations

--- Week5/LinkedListOperations.cpp
+++ Week5/LinkedListOperations.cpp
@@ -81,12 +81,9 @@ void display() {
 
     temporary = head;
     while (temporary != NULL) {
-        cout << temporary->data << " ";
+        cout << temporary->data << "->";
         temporary = temporary->nextNode;
     }
-
-    cout << endl << "-----------" << endl;
-
 }
 
 /*
@@ -102,6 +99,7 @@ void deleteNodeAtBeginning() {
     nodes--;
 }
 
+// Delete node at end
 void deleteNodeAtEnd() {
     temporary = head;
     while (temporary->nextNode != tail) {
@@ -114,6 +112,7 @@ void deleteNodeAtEnd() {
     nodes--;
 }
 
+// Delete node at a specific position
 void deleteNodeAtSpecificPosition(int position) {
     int first = 1;
 
@@ -135,8 +134,8 @@ void deleteNodeAtSpecificPosition(int position) {
     nodes--;
 }
 
+// search for a node with data
 void searchNode(int data) {
-
     temporary = head;
 
     while (temporary != NULL) {
@@ -147,38 +146,105 @@ void searchNode(int data) {
 
         temporary = temporary->nextNode;
     }
-
 }
 
 main() {
 
-    createList(5);
+    cout << "Create a linked list first: \n";
+    cout << "Enter the data: ";
 
-    insertNodeAtStart(4);
-    insertNodeAtStart(3);
-    insertNodeAtStart(2);
-    insertNodeAtStart(1);
+    int data;
+    cin >> data;
 
-    insertNodeAtEnd(6);
-    insertNodeAtEnd(7);
-    insertNodeAtEnd(8);
-    insertNodeAtEnd(9);
+    createList(data);
+    cout << "List created successfully!" << endl;
 
-    insertNodeAtSpecificPosition(3, 44);
-    display();
+    int choice;
+    while (true) {
 
-    deleteNodeAtBeginning();
-    display();
+        cout << "\n-----------" << endl;
+        cout << "1 - Insert at start" << endl;
+        cout << "2 - Insert at end" << endl;
+        cout << "3 - Insert at a specific position" << endl;
+        cout << "4 - Delete first node" << endl;
+        cout << "5 - Delete at specific position" << endl;
+        cout << "6 - Delete last node" << endl;
+        cout << "7 - Search a given value" << endl;
+        cout << "8 - Search number greater than given number" << endl;
+        cout << "9 - Display list" << endl;
+        cout << "10 - Quit" << endl;
+        cout << "Enter choice: " << endl;
+        cout << "-----------" << endl;
 
-    deleteNodeAtEnd();
-    display();
+        cin >> choice;
+        if (choice < 1 || choice > 10) {
+            cout << "Please enter a valid choice" << endl;
+        }
 
-    searchNode(8);
+        if (choice == 1) {
+            cout << "-----------" << endl;
+            cout << "Enter the data: ";
+            cin >> data;
+            insertNodeAtStart(data);
+            cout << "Node inserted at start successfully!";
+        }
 
-    insertNodeAtSpecificPosition(6, 32);
-    display();
+        if (choice == 2) {
+            cout << "-----------" << endl;
+            cout << "Enter the data: ";
+            cin >> data;
+            insertNodeAtEnd(data);
+            cout << "Node inserted at end successfully!";
+        }
 
-    deleteNodeAtSpecificPosition(5);
-    display();
+        if (choice == 3) {
+            cout << "-----------" << endl;
+            cout << "Enter the data: ";
+            cin >> data;
+            int position;
+            cout << "Enter the position: ";
+            cin >> position;
+            insertNodeAtSpecificPosition(position, data);
+            cout << "Node inserted at position " << position << " successfully!" << endl;
+        }
+
+        if (choice == 4) {
+            cout << "-----------" << endl;
+            deleteNodeAtBeginning();
+            cout << "Node deleted at start successfully!" << endl;
+        }
+
+        if (choice == 5) {
+            cout << "-----------" << endl;
+            int position;
+            cout << "Enter the position: ";
+            cin >> position;
+            deleteNodeAtSpecificPosition(position);
+            cout << "Node deleted at " << position << " successfully!" << endl;
+        }
+
+        if (choice == 6) {
+            cout << "-----------" << endl;
+            deleteNodeAtEnd();
+            cout << "Node deleted at end successfully!" << endl;
+        }
+
+        if (choice == 7) {
+            cout << "-----------" << endl;
+            cout << "Enter the value to search: " << endl;
+            cin >> data;
+            searchNode(data);
+        }
+
+        if (choice == 9) {
+            cout << "-----------" << endl;
+            display();
+        }
+
+        if (choice == 10) {
+            cout << "Exiting...";
+            break;
+        }
+    }
 
 }
